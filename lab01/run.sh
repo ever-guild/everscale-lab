78f8d0a9f22854dad0f192bcb1a9790a6f8a8c39
Ilyar <761285+ilyar@users.noreply.github.com>
2021-12-30 19:17:42 +0200

    update deps

--- lab01/run.sh
+++ lab01/run.sh
@@ -4,19 +4,19 @@ set -o errexit
 
 . ../util/util.sh
 
-npx tondev se reset
+npx everdev se reset
 rm -fr *.abi.json *.tvc
 
 # Deploy A
-npx tondev sol compile A.sol
-npx tondev contract deploy A --value 1000000000
+npx everdev sol compile A.sol
+npx everdev contract deploy A --value 1000000000
 
 # Deploy B
-npx tondev sol compile B.sol
-npx tondev contract deploy B --value 1000000000
+npx everdev sol compile B.sol
+npx everdev contract deploy B --value 1000000000
 addressB=$(addressContract B)
 echo "addressB ${addressB}"
 
-npx tondev contract run-local B doc
-npx tondev contract run A addToB --input "addr:${addressB},content:foo"
-npx tondev contract run-local B doc
+npx everdev contract run-local B doc
+npx everdev contract run A addToB --input "addr:${addressB},content:foo"
+npx everdev contract run-local B doc
--- lab02/run.sh
+++ lab02/run.sh
@@ -4,24 +4,24 @@ set -o errexit
 
 . ../util/util.sh
 
-npx tondev se reset
+npx everdev se reset
 rm -fr *.abi.json *.tvc
 
 # Deploy A - v1
-npx tondev sol compile A.sol
-npx tondev contract deploy A --value 1000000000
+npx everdev sol compile A.sol
+npx everdev contract deploy A --value 1000000000
 appAddress="0:$(addressContract A)"
 cp A.abi.json app.abi.json
 
 # Check version
-npx tondev contract run-local --address "${appAddress}" app version
+npx everdev contract run-local --address "${appAddress}" app version
 
 # Deploy B - v2
-npx tondev sol compile B.sol
+npx everdev sol compile B.sol
 input=$(printf 'code:%s' "$(npx tonos-cli decode stateinit --tvc B.tvc | tail -n +5 | jq -r .code)")
 echo "${input}"
-npx tondev contract run --address "${appAddress}" app upgrade --input "${input}"
+npx everdev contract run --address "${appAddress}" app upgrade --input "${input}"
 cp B.abi.json app.abi.json
 
 # Check version
-npx tondev contract run-local --address "${appAddress}" app version
+npx everdev contract run-local --address "${appAddress}" app version
